70de9bbb4020d55a923b50a03b35a1ecd92d1ac8
Mitch <mjmt997@gmail.com>
2019-01-25 20:04:44 +1100

    Planet connecters script

---
+++ GGJ2019/Assets/Src/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Planet : MonoBehaviour {
+    public int ZoneSize;
+    public List<GameObject> PlanetsInZone;
+
+    private void Start() {
+        Collider[] goPlanet = Physics.OverlapSphere(transform.position, ZoneSize);
+        for (int i = 0; i < goPlanet.Length; i++) {
+            if (goPlanet[i].GetComponent<Planet>() != null) PlanetsInZone.Add( goPlanet[i].gameObject);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, ZoneSize);
+    }
+}
