9375b5df28a0a478c7916d8f729d1e4eed389da0
Holiday Course <holiday@ad.aie.edu>
2019-01-26 21:06:35 +1100

    working explosion circle

---
+++ GGJ2019/Assets/ExplosionAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionAnimator : MonoBehaviour
+{
+    public float amount = 1.0f;
+    public ParticleSystem sparkParticleSystem;
+
+    public float minShapeRadius;
+    public float minStartLifetime;
+    public float minEmissionRateOverTime;
+	
+	void Awake ()
+    {
+        minShapeRadius = sparkParticleSystem.shape.radius;
+        minStartLifetime = sparkParticleSystem.startLifetime;
+        minEmissionRateOverTime = sparkParticleSystem.emission.rateOverTime.constant;
+
+        var shape = sparkParticleSystem.shape;
+        shape.radius = 0.25f;
+        transform.localScale = Vector3.one;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //sparkParticleSystem.startLifetime = amount * minStartLifetime * 0.5f;
+
+        //var emission = sparkParticleSystem.emission;
+        //emission.rateOverTime = amount * minEmissionRateOverTime;
+
+        //var shape = sparkParticleSystem.shape;
+        //shape.radius = amount * minShapeRadius;
+
+        transform.localScale = Vector3.one * amount;
+
+        amount += Time.deltaTime;
+    }
+}
