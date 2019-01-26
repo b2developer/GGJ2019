e331d0cc5a6175c675e9f64c57d9db18587f4e0d
Holiday Course <holiday@ad.aie.edu>
2019-01-26 19:33:29 +1100

    working modules with animations

---
+++ GGJ2019/Assets/FranciscoRomano/Player/ShipAnimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShipAnimator : MonoBehaviour
+{
+    private Animator[] animators;
+    
+	// Use this for initialization
+	void Awake() { animators = GetComponentsInChildren<Animator>(); Debug.Log(animators.Length); }
+	
+	public void EnableModule(int index) { animators[index].SetBool("isVisible", true); }
+
+    public void DisableModule(int index) { animators[index].SetBool("isVisible", false); }
+}
